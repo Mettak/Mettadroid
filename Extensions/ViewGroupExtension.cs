6d8475363d24707b0219a41414a7d4f6c1914bf4
Matěj Kubín <mettak123lol@gmail.com>
2021-03-13 17:05:29 +0100

    Added view group extension

---
+++ Extensions/ViewGroupExtension.cs
@@ -0,0 +1,45 @@
+﻿using Android.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mettarin.Android.Extensions
+{
+    public static class ViewGroupExtension
+    {
+        public static List<T> FindViewsByType<T>(this ViewGroup viewGroup, params int[] exceptions) where T : View
+        {
+            List<T> views = new List<T>();
+
+            for (int i = 0; i < viewGroup.ChildCount; i++)
+            {
+                var view = viewGroup.GetChildAt(i);
+
+                if (view is T targetView)
+                {
+                    if (exceptions.Contains(view.Id))
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        views.Add(targetView);
+                    }
+                }
+
+                else if (view is ViewGroup group)
+                {
+                    views.AddRange(FindViewsByType<T>(group));
+                }
+
+                else
+                {
+                    continue;
+                }
+            }
+
+            return views;
+        }
+    }
+}
